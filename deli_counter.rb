30c98fddb2e86a3d25124209ad58a369982a90a2
Robert Leduc <rleduc34@gmail.com>
2020-05-16 00:57:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+katz_deli = []
+
+def line(people)
+  if people.length == 0
+    puts "The line is currently empty."
+  elsif
+    puts "The line is currently #{people}"
+end
+end
